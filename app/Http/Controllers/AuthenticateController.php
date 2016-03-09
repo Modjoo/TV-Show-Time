16e9c5d7d7fc5c22309abe9e4926a09a33862ec7
Modjoo <chris3456789@hotmail.com>
2016-03-09 23:24:44 +0100

    work in progress

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -6,6 +6,7 @@ use App\Http\Requests;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Models\User;
 
 class AuthenticateController extends Controller
 {
@@ -18,14 +19,17 @@ class AuthenticateController extends Controller
     {
         $credentials = $request->only('pseudo', 'password');
 
+        $user = User::where('pseudo', $credentials['pseudo'])->where('password', Hash::make($credentials['password']))->get();
+
+        $token = JWTAuth::fromUser($user);
+
         try{
             // verify the credentials
             if(! $token = JWTAuth::attempt($credentials)){
                 return response()->json(['error' => 'invalid_credentials'], 401);
             }
         } catch (JWTException $e){
-            //return response()->json(['error' => 'could_not_create_token'], 500);
-            return pouet;
+            return response()->json(['error' => 'could_not_create_token'], 500);
         }
 
         // get the token
