3a57ea7621d91404eb6c02eb6f6c0fef509f8c3c
BARCHICHAT Emmanuel <Emmanuel.BARCHICHAT@cpnv.ch>
2016-06-01 15:01:27 +0200

    Add User_series table and functions in SerieController

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\DataBaseService;
 use App\Http\Services\JsonService;
 use App\Http\Services\SearchService;
+use App\Models\Series;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -18,20 +20,19 @@ class SerieController extends Controller
     public function __construct()
     {
         $this->extservice = new SearchService();
-        //$this->dbservice = new DataBaseService();
+        $this->dbservice = new DataBaseService();
     }
 
     public function searchSerie($string){
-        $series = $this->extservice->searchSeriesByName($string);
-        return JsonService::generateSeries($series);
+        return JsonService::generateSeries($this->extservice->searchSeriesByName($string));
     }
 
-    public function getSerie($idserie){
-
+    public function getSerie($idSerie){
+        return json_encode(["serie" => Series::where("id", "=", $idSerie)->get()]);
     }
 
-    public function getEpisode(){
-
+    public function getEpisode($idEpisode){
+        return json_encode(["episode" => Episode::where("id", "=", $idEpisode)->get()]);
     }
 
     public function subscribe(){
---
+++ app/Models/UsersSeries.php
@@ -0,0 +1,21 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GenresSeries extends Model {
+
+    protected $table = 'users_series';
+    protected $fillable = ['id', 'user_id', 'serie_id'];
+    public $timestamps = false;
+
+
+    public function user() {
+        return $this->belongsTo(\App\Models\Genre::class, 'user_id', 'id');
+    }
+
+    public function series() {
+        return $this->belongsTo(\App\Models\Series::class, 'serie_id', 'id');
+    }
+
+
+}
